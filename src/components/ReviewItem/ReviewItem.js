e85674931bfc7546aa71c4f05a8194a53eb6aba1
Amir Hossain <amir508_du@yahoo.com>
2022-04-05 03:07:46 +0600

    images loaded and review button added

--- src/components/ReviewItem/ReviewItem.js
+++ src/components/ReviewItem/ReviewItem.js
@@ -3,7 +3,6 @@ import React from 'react';
 const ReviewItem = ({review}) => {
     const {name, review: reviewText, image, rating} = review;
 
-    // console.log(review)
     return (
         <>
         <section className="text-gray-600 body-font">
@@ -11,11 +10,11 @@ const ReviewItem = ({review}) => {
                 <div className="flex flex-wrap -m-4">
                     <div className="lg:w-full lg:mb-0 mb-6 p-4">
                         <div className="h-full text-center">
-                        <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src={image}/>
-                        <p className="leading-relaxed">{reviewText}</p>
-                        <span className="inline-block h-1 w-10 rounded bg-indigo-500 mt-6 mb-4"></span>
-                        <p className="text-gray-900 font-medium title-font tracking-wider text-sm">{name}</p>
-                        <p className="text-gray-500">Rating: {rating}</p>
+                            <img alt="testimonial" className="w-40 h-40 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src={image}/>
+                            <p className="leading-relaxed">{reviewText}</p>
+                            <span className="inline-block h-1 w-10 rounded bg-indigo-500 mt-6 mb-4"></span>
+                            <p className="text-gray-900 font-medium title-font tracking-wider text-sm">{name}</p>
+                            <p className="text-gray-500">Rating: {rating}</p>
                         </div>
                     </div>
                 </div>
