0b63809919e7acf2cfd05202cd63e86ca547b0cc
Amir Hossain <amir508_du@yahoo.com>
2022-04-04 07:08:31 +0600

    dashboard and blogs component added

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -2,9 +2,24 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <div>
-            <h1>This is blog page</h1>
-        </div>
+        <section className="text-gray-600 body-font overflow-hidden">
+            <div className="container px-5 py-24 mx-auto">
+                <div className="-my-8 divide-y-2 divide-gray-100">
+                    <div className="py-8 md:flex-grow">
+                        <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">What is Context API? What are the uses of it?</h2>
+                        <p className="leading-relaxed">Glossier echo park pug, church-key sartorial biodiesel vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon party messenger bag selfies, poke vaporware kombucha lumbersexual pork belly polaroid hoodie portland craft beer.</p>
+                    </div>
+                    <div className="py-8 md:flex-grow">
+                        <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">What is Semantic tag. Give some example.</h2>
+                        <p className="leading-relaxed">Glossier echo park pug, church-key sartorial biodiesel vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon party messenger bag selfies, poke vaporware kombucha lumbersexual pork belly polaroid hoodie portland craft beer.</p>
+                    </div>
+                    <div className="py-8 md:flex-grow">
+                        <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">Difference between inline, block, inline-block elements</h2>
+                        <p className="leading-relaxed">Glossier echo park pug, church-key sartorial biodiesel vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon party messenger bag selfies, poke vaporware kombucha lumbersexual pork belly polaroid hoodie portland craft beer.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
+import { Legend, Line, LineChart, Pie, PieChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Dashboard = () => {
     const [dashboard, setDashboard] = useState([]);
@@ -12,17 +12,27 @@ const Dashboard = () => {
     // console.log(dashboard)
 
     return (
-        <div>
+        <div className='grid grid-cols-2'>
             <div>
-                <h1 className='my-6 text-4xl '>Monthly Sales</h1>
-                <ResponsiveContainer width='50%' height={500}>
-                    <LineChart width={800} height={500} data={dashboard}>
+                <h1 className='my-6 text-4xl'>Monthly Sales</h1>
+                <ResponsiveContainer width='50%' height={350}>
+                    <LineChart width={800} height={350} data={dashboard}>
                         <Line dataKey='sell'></Line>
                         <XAxis dataKey='month'></XAxis>
                         <YAxis dataKey='sell'></YAxis>
                         <Tooltip />
                         <Legend verticalAlign='top' height={36} />
                     </LineChart>
+                </ResponsiveContainer>
+            </div>
+            <div>
+                <h1 className='my-6 text-4xl'>Investment vs Revenue</h1>
+                <ResponsiveContainer width='50%' height={500}>
+                    <PieChart width={800} height={350}>
+                        <Pie data={dashboard} dataKey="investment" nameKey="name" cx="50%" cy="50%" outerRadius={50} fill="#8884d8" />
+                        <Pie data={dashboard} dataKey="revenue" nameKey="name" cx="50%" cy="50%" innerRadius={60} outerRadius={80} fill="#82ca9d" label />
+                        <Tooltip />
+                    </PieChart>
                 </ResponsiveContainer>
             </div>
         </div>
